9991789bf73ab10fe2848dc23a2401f3ea3a07b7
ShenXu8888 <90271342+ShenXu8888@users.noreply.github.com>
2021-09-20 16:08:56 +0100

    Update creat.sql

--- sqlscripts/creat.sql
+++ sqlscripts/creat.sql
@@ -1,9 +1,1 @@
-USE [CaboodlePOC_Stage]
-
-create table Custom.SXu_ErsatzEmpolyeeRole (	StageKey				int IDENTITY(1,1)	--Technically optional	
-					    ,eMPLOYEE_ROLE_ID					nvarchar(10)
-	,eMPLOYEE_ROLE_NAME					nvarchar(50)
-	,EMPLOYEE_ROLE_DESCRIPTION				nvarchar(300)
-	,EMPLOYEE_CLASS				nvarchar(100)
-	,LoadDate					date
-	)
+with a as (select 1 as b) select * from a cros join c
